2e10b42c8ddd43f73fb0dc142a837f2c6efa2930
quebox <xcrwm8@gmail.com>
2024-07-31 19:23:23 +0900

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,11 @@
-fetch('navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading navbar:', error));
+// load-navbar.js
+function loadNavbar() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+}
+
+document.addEventListener('DOMContentLoaded', loadNavbar);
